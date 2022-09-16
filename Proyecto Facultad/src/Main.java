67f0909255a862ead20175a418ef9e43706902b7
Maximiliano Acosta <111078580+MaxiAcostaGIT@users.noreply.github.com>
2022-09-16 18:51:44 -0300

    Push desde IDE

--- Proyecto Facultad/src/Main.java
+++ Proyecto Facultad/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Esto es una Prueba...");
         System.out.println("Esto es una Prueba... push desde IDE");
+        System.out.println("Esto es una Prueba... pull desde IDE");
     }
 }
